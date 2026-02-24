5a7656180bd568d464d4d2ef3c0c487efabb2abd
NazmusSakib24 <ahmednazmussakib629@gmail.com>
2026-02-24 14:52:53 +0600

    name page validation

---
+++ LAB-2/script(name).js
@@ -0,0 +1,10 @@
+function nam_val(){
+    let username=document.getElementById('name');
+    
+    if (username.value== ""){
+        document.getElementById('msg').innerHTML="Can not be Empty";
+    }
+    else{
+        
+    }
+}
