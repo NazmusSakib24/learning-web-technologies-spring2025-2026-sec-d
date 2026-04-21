5019726a731ba10c89f15b87929e7dc6c93725db
NazmusSakib24 <ahmednazmussakib629@gmail.com>
2026-04-21 15:55:23 +0600

    created login.php

---
+++ Final Term/LAB-1/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login <Form></Form></title>
+
+    <style>
+        .div1{
+            display: flex;
+            /* justify-content : center; */
+            /* align-items : center ; */
+            margin: auto;
+            padding : auto;
+           /* width: 100px; */
+            /* height : 600px; */
+        }
+    </style>
+</head>
+<body>
+    <div class="div1">
+        <fieldset>
+        <legend>LOGIN</legend>
+        <form action="login_val.php" method="post">
+            
+            <label>User Name:</label>
+            
+            <input type="text" name="username">
+            <br><br>
+
+            <label>Password:</label>
+            <input type="password" name="password">
+            <br><br>
+
+            <input type="checkbox" name="check" id=""> Remember
+            <br><br>
+
+            <button type="button">Submit</button>
+            <a>Forgot Password</a>
+        </form>
+    </fieldset>
+    </div>
+</body>
+</html>
