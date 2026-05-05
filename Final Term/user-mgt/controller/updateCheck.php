b3d10d1c6e5292df6adba506df8c846e3941811d
NazmusSakib24 <ahmednazmussakib629@gmail.com>
2026-05-05 16:14:13 +0600

    edit

--- Final Term/user-mgt/controller/updateCheck.php
+++ Final Term/user-mgt/controller/updateCheck.php
@@ -1,5 +1,33 @@
 <?php
-    session_start();
+session_start();
+
+if (isset($_POST['submit'])) {
+
+    $id = $_POST['id'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+
+    if (isset($_SESSION['users'])) {
+
+        $users = $_SESSION['users'];
+
+        foreach ($users as $key => $u) {
+            if ($u['id'] == $id) {
+                $users[$key]['username'] = $username;
+                $users[$key]['email'] = $email;
+            }
+        }
+
+        $_SESSION['users'] = $users;
+    }
+
+    header("Location: ../view/user_list.php");
+    exit();
+}
+else {
+    echo "Invalid Request!";
+}
+?>
 
     
 
